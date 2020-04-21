a90b5df64c1ee7ab1f4df808c49790abf9f73368
Shubham Sanjay Jain <itshubhamjain@gmail.com>
2020-04-21 01:44:16 -0400

    Done MF Structures in finalWrite

---
+++ project/output/output.py
@@ -0,0 +1,91 @@
+class Relation:
+	cust = ''
+	def getCust(self):
+		return self.cust
+	def setCust(self,cust):
+		self.cust = cust
+
+	prod = ''
+	def getProd(self):
+		return self.prod
+	def setProd(self,prod):
+		self.prod = prod
+
+	day =  0
+	def getDay(self):
+		return self.day
+	def setDay(self,day):
+		self.day = day
+
+	month =  0
+	def getMonth(self):
+		return self.month
+	def setMonth(self,month):
+		self.month = month
+
+	year =  0
+	def getYear(self):
+		return self.year
+	def setYear(self,year):
+		self.year = year
+
+	state = ''
+	def getState(self):
+		return self.state
+	def setState(self,state):
+		self.state = state
+
+	quant =  0
+	def getQuant(self):
+		return self.quant
+	def setQuant(self,quant):
+		self.quant = quant
+
+
+class MF_Structure:
+	prod = ''
+	def getProd(self):
+		return self.prod
+	def setProd(self,prod):
+		self.prod = prod
+	month =  0
+	def getMonth(self):
+		return self.month
+	def setMonth(self,month):
+		self.month = month
+
+	_1_count_quant =  None
+	def get_1_count_quant(self):
+		return self._1_count_quant
+	def set_1_count_quant(self,_1_count_quant):
+		self._1_count_quant = _1_count_quant
+
+	_1_sum_quant =  None
+	def get_1_sum_quant(self):
+		return self._1_sum_quant
+	def set_1_sum_quant(self,_1_sum_quant):
+		self._1_sum_quant = _1_sum_quant
+
+	_2_count_quant =  None
+	def get_2_count_quant(self):
+		return self._2_count_quant
+	def set_2_count_quant(self,_2_count_quant):
+		self._2_count_quant = _2_count_quant
+
+	_2_sum_quant =  None
+	def get_2_sum_quant(self):
+		return self._2_sum_quant
+	def set_2_sum_quant(self,_2_sum_quant):
+		self._2_sum_quant = _2_sum_quant
+
+	_3_count_quant =  None
+	def get_3_count_quant(self):
+		return self._3_count_quant
+	def set_3_count_quant(self,_3_count_quant):
+		self._3_count_quant = _3_count_quant
+
+
+def main():
+
+if __name__ =='__main__':
+	main()
